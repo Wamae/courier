0e4bdf90622c490abb63a02a734d3cd567a10a2f
Wamae <wamaebenson06@gmail.com>
2017-11-24 11:48:24 +0300

    Waybill title update

--- resources/views/waybills/index.blade.php
+++ resources/views/waybills/index.blade.php
@@ -4,7 +4,7 @@
 
 <div class="panel panel-default">
     <div class="panel-heading">
-        List of {{ ucfirst('waybills') }}
+        {{ ucfirst('waybill') }} Information Entry Management
     </div>
 
     <div class="panel-body">
