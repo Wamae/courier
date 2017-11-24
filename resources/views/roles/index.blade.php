27d5b950b50f6203a53f239b4d08d74e86630db6
Wamae <wamaebenson06@gmail.com>
2017-11-24 08:31:01 +0300

    Roles: Update to datatable

--- resources/views/roles/index.blade.php
+++ resources/views/roles/index.blade.php
@@ -8,11 +8,12 @@
 <div class="col-lg-10 col-lg-offset-1">
     <h1><i class="fa fa-key"></i> Roles
 
-    <a href="{{ route('users.index') }}" class="btn btn-default pull-right">Users</a>
-    <a href="{{ route('permissions.index') }}" class="btn btn-default pull-right">Permissions</a></h1>
+    <a href="{{ route('users.index') }}" class="btn btn-info">Users</a>
+    <a href="{{ route('permissions.index') }}" class="btn btn-info">Permissions</a>
+    </h1>
     <hr>
-    <div class="table-responsive">
-        <table class="table table-bordered table-striped">
+    <div class="">
+        <table id="thegrid" class="table table-bordered table-striped">
             <thead>
                 <tr>
                     <th>Role</th>
@@ -46,5 +47,18 @@
     <a href="{{ URL::to('roles/create') }}" class="btn btn-success">Add Role</a>
 
 </div>
+@endsection
+@section('scripts')
+    <script type="text/javascript">
+        var theGrid = null;
+        $(document).ready(function(){
+            theGrid = $('#thegrid').DataTable({
+                "processing": false,
+                "serverSide": false,
+                "ordering": true,
+                "responsive": true
+            });
+        });
 
+    </script>
 @endsection
